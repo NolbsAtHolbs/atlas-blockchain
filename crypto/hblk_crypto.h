d26651c9383bf2c9f60ce3079696b965ba985cf8
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-23 11:29:05 -0600

    beginning cryptography

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,1 +1,18 @@
+#ifndef HBLK_CRYPTO_H
+#define HBLK_CRYPTO_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <openssl/ssl.h>
+#include <openssl/sha.h>
+
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
+
+
+#endif
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,1 +1,18 @@
+#include "hblk_crypto.h"
 
+/*
+* sha256 - computes the hash of a sequence of bytes
+* @s: sequence of bytes to be hashed
+* @len: number of bytes to hash in s
+* @digest: storage for resulting hash
+* Return: pointer to digest
+*/
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+	if (!digest)
+		return (NULL);
+	if(s)
+		SHA256((unsigned char const *)s, len, digest);
+	return (digest);
+}
