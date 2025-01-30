c83ca08ee5619a23409570d3742f8d47aaf8b0fa
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-29 22:24:15 -0600

    sharp test

---
+++ crypto/ec_to_pub.c
@@ -0,0 +1,28 @@
+#include "hblk_crypto.h"
+
+/*
+* ec_to_pub - extracts the public key from an EC_KEY opaque struct
+* @key: pointer to the EC_KEY struct to retrieve the pubkey from
+* @pub: address at which to store the extracted public key
+* Return: pointer to pub
+*/
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
+{
+	const EC_GROUP *group;
+	const EC_POINT *pblc_point;
+	size_t len;
+
+	if (!key || !pub)
+		return (NULL);
+	group = EC_KEY_get0_group(key);
+	if (!group)
+		return (NULL);
+	pblc_point = EC_KEY_get0_public_key(key);
+	if (!pblc_point)
+		return (NULL);
+	len = EC_POINT_point2oct(group, pblc_point, POINT_CONVERSION_UNCOMPRESSED,
+							 pub, EC_PUB_LEN, NULL);
+	if (len != EC_PUB_LEN)
+		return (NULL);
+	return (pub);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -9,11 +9,17 @@
 #include <unistd.h>
 #include <openssl/ssl.h>
 #include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/pem.h>
+#include <openssl/ecdsa.h>
+#include <openssl/obj_mac.h>
 
+#define EC_PUB_LEN 65 /* uncompressed pblc key: 0x04 + 32-byte X + 32-byte Y */
 
 uint8_t *sha256(int8_t const *s, size_t len,
 				uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 
 #endif
