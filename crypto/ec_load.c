74337ed6c5bfa1e8b12a632e77d6977d5566df82
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-29 23:23:42 -0600

    gotta handle both key and key_pub

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -7,20 +7,41 @@
  */
 EC_KEY *ec_load(char const *folder)
 {
-	FILE *fp = NULL;
 	char path[1024];
-	EC_KEY *key = NULL;
-	int len;
+	FILE *fp;
+	EC_KEY *key = NULL, *tmp = NULL;
 
 	if (!folder)
-		return (NULL);
-	len = snprintf(path, sizeof(path), "%s/key.pem", folder);
-	if (len < 0 || len >= (int)sizeof(path))
-		return (NULL);
+		return (NULL); /* read the PRIVATE KEY from: <folder>/key.pem */
+	snprintf(path, sizeof(path), "%s/key.pem", folder);
 	fp = fopen(path, "r");
 	if (!fp)
 		return (NULL);
 	key = PEM_read_ECPrivateKey(fp, NULL, NULL, NULL);
 	fclose(fp);
+	if (!key)
+		return (NULL); /* read the PUBLIC KEY from: <folder>/key_pub.pem */
+	snprintf(path, sizeof(path), "%s/key_pub.pem", folder);
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	if (!PEM_read_EC_PUBKEY(fp, &tmp, NULL, NULL))
+	{
+		fclose(fp);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	fclose(fp);
+	/* combine by setting public key from tmp into our prvt key structure */
+	if (EC_KEY_set_public_key(key, EC_KEY_get0_public_key(tmp)) != 1)
+	{
+		EC_KEY_free(tmp);
+		EC_KEY_free(key);
+		return (NULL);
+	} /* release temporary key; 'key' now holds both private + public parts */
+	EC_KEY_free(tmp);
 	return (key);
 }
