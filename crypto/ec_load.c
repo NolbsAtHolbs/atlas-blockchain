e7de566daf1fc1be941f1e4c5f3902caaaef7559
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-29 23:12:39 -0600

    try with len

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,25 +1,26 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_load - Loads an EC key pair from disk
- * @folder: Path to the folder containing the key files
- * Return: Pointer to the EC_KEY structure, or NULL on failure
+ * ec_load - loads an EC key pair from disk
+ * @folder: path to the folder containing the key files
+ * Return: pointer to the EC_KEY structure, or NULL on failure
  */
 EC_KEY *ec_load(char const *folder)
 {
+	FILE *fp = NULL;
 	char path[1024];
-	FILE *fp;
 	EC_KEY *key = NULL;
+	int len;
 
 	if (!folder)
-		return (NULL); /* build the full path to <folder>/key.pem */
-	snprintf(path, sizeof(path), "%s/%s", folder, "key.pem");
+		return (NULL);
+	len = snprintf(path, sizeof(path), "%s/key.pem", folder);
+	if (len < 0 || len >= (int)sizeof(path))
+		return (NULL);
 	fp = fopen(path, "r");
 	if (!fp)
-		return (NULL); /* read prvt key in PEM format; yields full key pair */
+		return (NULL);
 	key = PEM_read_ECPrivateKey(fp, NULL, NULL, NULL);
 	fclose(fp);
-	if (!key)
-		return (NULL);
 	return (key);
 }
