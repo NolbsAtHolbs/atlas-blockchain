3210c29e74df0502ab15c27a6deae48714ee7ad0
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-02-12 23:44:53 -0600

    really slow genesis validation

---
+++ blockchain/v0.1/block_is_valid.c
@@ -0,0 +1,53 @@
+#include "blockchain.h"
+
+/**
+ * block_is_valid - Validates a block in the blockchain
+ * @block: Pointer to the block to validate
+ * @prev_block: Pointer to the previous block in the chain
+ *
+ * Return: 0 if valid, -1 otherwise
+ */
+int block_is_valid(block_t const *block, block_t const *prev_block)
+{
+	uint8_t computed_hash[SHA256_DIGEST_LENGTH];
+
+	int i;
+
+	if (!block)
+		return (-1);
+	/* validate genesis Block */
+	if (block->info.index == 0)
+	{
+		if (prev_block)
+			return (-1);
+		if (block->data.len != 16 ||
+			memcmp(block->data.buffer, "Holberton School", 16) != 0)
+			return (-1);
+		for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+			if (block->info.prev_hash[i] != 0)
+				return (-1);
+		if (memcmp(block->hash, ATLAS_SCHOOL_SHA256, SHA256_DIGEST_LENGTH) != 0)
+			return (-1);
+	}
+	else
+	{
+		/* validate non-genesis Block */
+		if (!prev_block)
+			return (-1);
+		if (block->info.index != prev_block->info.index + 1)
+			return (-1);
+		if (memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
+			return (-1);
+	}
+
+
+	if (block->data.len > BLOCKCHAIN_DATA_MAX)
+		return (-1);
+
+	/* validate hash */
+	block_hash(block, computed_hash);
+	if (memcmp(block->hash, computed_hash, SHA256_DIGEST_LENGTH) != 0)
+		return (-1);
+
+	return (0);
+}
