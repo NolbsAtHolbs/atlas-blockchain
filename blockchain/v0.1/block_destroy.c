6240750c2d718adc68ecf0ed8d7b246d7f62d610
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-02-12 20:55:15 -0600

    1 obj free

---
+++ blockchain/v0.1/block_destroy.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+
+/*
+ * block_destroy - frees the memory used by a block
+ * @block: pointer to the block to delete
+ */
+void block_destroy(block_t *block)
+{
+	if (!block)
+		return;
+	/* can free allocated sub-objs before freeing block if applicable */
+	free(block);
+}
