bb02ffae5b842a0aad9a641447306bb77d34ff85
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-02-20 10:36:49 -0600

    mine pointer, no actual mining

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,19 @@
+#include "blockchain.h"
+
+/*
+ * block_mine - finds a nonce so that block->hash meets block->info.difficulty
+ * @block: pointer to the block to be mined
+ */
+void block_mine(block_t *block)
+{
+	if (!block)
+		return;
+	block->info.nonce = 0;
+	while (1)
+	{
+		block_hash(block, block->hash);
+		if (hash_matches_difficulty(block->hash, block->info.difficulty))
+			break;
+		block->info.nonce++;
+	}
+}
