b9f022472119c07fc00851f3b79aef2423b1d412
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-20 00:06:47 -0500

    trying wrapper for pesky llist_for_each signature

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -11,14 +11,21 @@ static const block_t GENESIS_BLOCK = {
 	{ /* data */
 		"Holberton School", /* buffer */
 		16                  /* length */
-	}, /* hash -> 32-byte SHA-256 result */
-	ATLAS_SCHOOL_SHA256
+	},
+	NULL, /* Initialize transactions to NULL */
+	ATLAS_SCHOOL_SHA256 /* Set hash */
 };
 
+
+static int validate_tx_wrapper(void *node, unsigned int index, void *arg)
+{
+	return transaction_is_valid((transaction_t *)node, (llist_t *)arg);
+}
+
 /**
- * block_is_valid - Validates a block in the blockchain
- * @block: Pointer to the block to validate
- * @prev_block: Pointer to the previous block in the chain
+ * block_is_valid - checks the validity of a block
+ * @block: pointer to block to validate
+ * @prev_block: pointer to previous block
  * @all_unspent: list of all unspent transaction outputs
  * Return: 0 if valid, -1 otherwise
  */
@@ -26,45 +33,41 @@ int block_is_valid(block_t const *block, block_t const *prev_block,
 				   llist_t *all_unspent)
 {
 	uint8_t computed_hash[SHA256_DIGEST_LENGTH];
-	uint8_t prev_hash_computed[SHA256_DIGEST_LENGTH];
-	transaction_t *coinbase = NULL;
-	size_t tx_count = 0;
+	uint8_t prev_hash[SHA256_DIGEST_LENGTH];
 
 	if (!block || !all_unspent)
 		return (-1);
 
 	if (block->info.index == 0)
-	{ /* genesis block checks */
-		if (prev_block)
-			return (-1);
-		if (memcmp(block, &GENESIS_BLOCK, sizeof(GENESIS_BLOCK)) != 0)
+	{ /* validate genesis block */
+		if (prev_block || memcmp(block, &GENESIS_BLOCK,
+				sizeof(GENESIS_BLOCK)))
 			return (-1);
 	}
 	else
-	{ /* non-genesis block checks */
-		if (!prev_block)
+	{ /* validate regular block */
+		if (!prev_block || block->info.index != prev_block->info.index + 1)
 			return (-1);
-		if (block->info.index != prev_block->info.index + 1)
+		block_hash(prev_block, prev_hash);
+		if (memcmp(prev_block->hash, prev_hash, SHA256_DIGEST_LENGTH) != 0 ||
+			memcmp(block->info.prev_hash, prev_hash,
+					SHA256_DIGEST_LENGTH) != 0)
 			return (-1);
-		block_hash(prev_block, prev_hash_computed);
-		if (memcmp(prev_block->hash, prev_hash_computed,
-			SHA256_DIGEST_LENGTH) != 0)
-			return (-1);
-		if (memcmp(block->info.prev_hash, prev_block->hash,
-			SHA256_DIGEST_LENGTH) != 0)
-			return (-1);
-	} /* check that the block contains at least one tx */
-	tx_count = llist_size(block->transactions);
-	if (tx_count < 1)
+	}
+
+	if (block->data.len > BLOCKCHAIN_DATA_MAX) /* check block data size */
 		return (-1);
-	coinbase = llist_get_head(block->transactions); /* 1st tx - valid coinbs */
-	if (!coinbase || !coinbase_is_valid(coinbase, block->info.index))
-		return (-1);
-	if (llist_for_each(block->transactions, /* validate all other txs */
-			(node_func_t)transaction_is_valid, all_unspent) != 0)
-		return (-1);
-	block_hash(block, computed_hash);
+
+	block_hash(block, computed_hash); /* validate block hash */
 	if (memcmp(block->hash, computed_hash, SHA256_DIGEST_LENGTH) != 0)
+		return (-1);
+	/* validate txs */
+	if (!block->transactions || llist_size(block->transactions) < 1)
+		return (-1);
+	if (coinbase_is_valid(llist_get_head(block->transactions),
+						  block->info.index) == 0)
+		return (-1);
+	if (llist_for_each(block->transactions, validate_tx_wrapper, all_unspent) != 0)
 		return (-1);
 	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		return (-1);
