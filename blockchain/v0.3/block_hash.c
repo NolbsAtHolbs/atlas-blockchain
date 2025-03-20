a48a28e2f14f93e0f9ef089b975f2991d09fffd3
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-19 23:39:16 -0500

    copied tx info and data into buffer

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -1,7 +1,7 @@
 #include "blockchain.h"
 
 /**
- * block_hash - computes the hash of a block's info and data
+ * block_hash - computes the hash of a block's info, data, and transactions
  * @block: pointer to the block to hash (unchanged)
  * @hash_buf: buffer where the resulting hash will be stored
  * Return: the hashed buffer
@@ -9,22 +9,43 @@
 uint8_t *block_hash(block_t const *block,
 					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	uint8_t buf[sizeof(block->info) + BLOCKCHAIN_DATA_MAX];
-	size_t len;
+	uint8_t *buf;
+	size_t len, tx_len = 0;
+	transaction_t *tx;
+	int i;
 
 	if (!block || !hash_buf)
 		return (NULL);
 
-	/* copy the block info into buf first, add block->data content right after */
-	memcpy(buf, &block->info, sizeof(block->info));
+	/* compute size of all txs */
+	for (i = 0; i < llist_size(block->transactions); i++)
+	{
+		tx = llist_get_node_at(block->transactions, i);
+		if (tx)
+			tx_len += sizeof(*tx);
+	}
 
-	/* 'data.len' informs how many bytes in 'data.buffer' to include */
-	/* can't exceed BLOCKCHAIN_DATA_MAX when copying */
-	len = sizeof(block->info) + block->data.len;
+	/* allocate memory for block info + data + txs */
+	len = sizeof(block->info) + block->data.len + tx_len;
+	buf = malloc(len);
+	if (!buf)
+		return (NULL);
+
+	/* copy block info and data into the buffer */
+	memcpy(buf, &block->info, sizeof(block->info));
 	memcpy(buf + sizeof(block->info), block->data.buffer, block->data.len);
 
-	/* compute SHA-256 on that combined info+data, store result into 'hash_buf' */
-	SHA256((const unsigned char *)buf, len, hash_buf);
+	/* copy tx data into buffer */
+	tx_len = sizeof(block->info) + block->data.len;
+	for (i = 0; i < llist_size(block->transactions); i++)
+	{
+		tx = llist_get_node_at(block->transactions, i);
+		if (tx)
+			memcpy(buf + tx_len, tx, sizeof(*tx)), tx_len += sizeof(*tx);
+	}
 
+	/* compute SHA-256 on combined info + data + txs */
+	SHA256((const unsigned char *)buf, len, hash_buf);
+	free(buf);
 	return (hash_buf);
 }
