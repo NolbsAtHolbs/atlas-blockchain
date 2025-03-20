723d5f51bda03454b4de7f81d387bc56a4f3e8b4
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-19 23:35:00 -0500

    betty

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -6,29 +6,41 @@
  */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *bc = calloc(1, sizeof(*bc));
+	blockchain_t *bc;
 	block_t *block;
 
-	/* allocate blockchain and create lists for chain and unspent outputs */
-	if (!bc || !(bc->chain = llist_create(MT_SUPPORT_FALSE)) ||
-		!(bc->unspent = llist_create(MT_SUPPORT_FALSE)))
+	bc = calloc(1, sizeof(*bc)); /* allocate blockchain */
+	if (!bc)
+		return (NULL);
+
+	/* create linked list for blocks */
+	bc->chain = llist_create(MT_SUPPORT_FALSE);
+	if (!bc->chain)
 		return (free(bc), NULL);
 
-	block = calloc(1, sizeof(*block));
-	if (!block) /* free everything if block allocation fails */
-		return (llist_destroy(bc->chain, 0, NULL),
-			llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
+	/* create linked list for unspent tx outputs */
+	bc->unspent = llist_create(MT_SUPPORT_FALSE);
+	if (!bc->unspent)
+		return (llist_destroy(bc->chain, 0, NULL), free(bc), NULL);
 
-	/* initialize genesis block fields */
+	/* allocate memory for genesis block */
+	block = calloc(1, sizeof(*block));
+	if (!block)
+		return (llist_destroy(bc->chain, 0, NULL),
+				llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
+
+	/* initialize genesis block info */
 	block->info = (block_info_t){0, 0, 1537578000, 0, {0}};
+	/* Initialize genesis block data */
 	block->data.len = 16;
-	memcpy(block->data.buffer, "Holberton School", 16); /* fixed 16-byte str */
+	memcpy(block->data.buffer, "Holberton School", 16);
+	/* set predefined hash for genesis block */
 	memcpy(block->hash, ATLAS_SCHOOL_SHA256, SHA256_DIGEST_LENGTH);
 
-	/* add genesis block to chain; free on failure */
+	/* add genesis block to chain */
 	if (llist_add_node(bc->chain, block, ADD_NODE_REAR) == -1)
 		return (free(block), llist_destroy(bc->chain, 0, NULL),
-			llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
+				llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
 
 	return (bc);
 }
