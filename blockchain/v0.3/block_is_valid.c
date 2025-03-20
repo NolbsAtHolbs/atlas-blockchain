a60b26ffe440a0724d7928a42980cfcfee280238
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-20 00:18:07 -0500

    retry

--- blockchain/v0.3/block_is_valid.c
+++ blockchain/v0.3/block_is_valid.c
@@ -16,13 +16,12 @@ static const block_t GENESIS_BLOCK = {
 	ATLAS_SCHOOL_SHA256 /* Set hash */
 };
 
-
 static int validate_tx_wrapper(void *node, unsigned int index, void *arg)
 {
 	(void)index;
-	return transaction_is_valid((transaction_t *)node, (llist_t *)arg);
+	return (transaction_is_valid((const transaction_t *)node,
+			(llist_t *)arg) ? 0 : 1);
 }
-
 /**
  * block_is_valid - checks the validity of a block
  * @block: pointer to block to validate
@@ -39,35 +38,31 @@ int block_is_valid(block_t const *block, block_t const *prev_block,
 	if (!block || !block->transactions || llist_size(block->transactions) < 1)
 		return (-1);
 
-	/* if genesis block, validate with known genesis data */
 	if (block->info.index == 0)
-		return (memcmp(block, &GENESIS_BLOCK,
-			sizeof(GENESIS_BLOCK)) == 0 ? 0 : -1);
+		return (memcmp(block, &GENESIS_BLOCK, sizeof(GENESIS_BLOCK)) == 0 ? 0 : -1);
 
 	if (!prev_block || block->info.index != prev_block->info.index + 1)
 		return (-1);
 
 	block_hash(prev_block, computed_hash);
 	if (memcmp(prev_block->hash, computed_hash, SHA256_DIGEST_LENGTH) != 0 ||
-		memcmp(block->info.prev_hash, computed_hash,
-			SHA256_DIGEST_LENGTH) != 0)
+		memcmp(block->info.prev_hash, computed_hash, SHA256_DIGEST_LENGTH) != 0)
 		return (-1);
 
-	coinbase_tx = llist_get_head(block->transactions); /* valid8 coinbase tx */
+	coinbase_tx = llist_get_head(block->transactions);
 	if (!coinbase_tx || !coinbase_is_valid(coinbase_tx, block->info.index))
 		return (-1);
 
-	/* validate all other txs */
-	if (llist_for_each(block->transactions, (node_func_t)validate_tx_wrapper,
-		all_unspent) != 0)
+	if (llist_for_each_from(block->transactions, 1,
+							(node_func_t)validate_tx_wrapper, all_unspent) != 0)
 		return (-1);
 
-	block_hash(block, computed_hash); /* check block hash matches */
+	block_hash(block, computed_hash);
 	if (memcmp(block->hash, computed_hash, SHA256_DIGEST_LENGTH) != 0)
 		return (-1);
 
-	/* check difficulty */
 	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		return (-1);
+
 	return (0);
 }
