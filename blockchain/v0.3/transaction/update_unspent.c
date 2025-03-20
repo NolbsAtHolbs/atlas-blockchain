21d8c63aa9e988e9f5a9ac10200d1b979318d1ba
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-20 00:32:51 -0500

    unbork

--- blockchain/v0.3/transaction/update_unspent.c
+++ blockchain/v0.3/transaction/update_unspent.c
@@ -1,35 +1,29 @@
 #include "blockchain.h"
 
-static int remove_spent_unspent(void *node, unsigned int idx, void *arg);
+static int remove_spent_unspent(void *node, void *arg);
 static int add_new_unspent(void *node, unsigned int idx, void *arg);
 
 /**
  * add_new_unspent - adds new unspent transaction outputs to list
  * @node: current transaction being processed
- * @idx: index of the transaction
+ * @index: index of the transaction
  * @arg: pointer to a struct containing block hash and new unspent list
  * Return: 0 on success, -1 on failure
  */
-static int add_new_unspent(void *node, unsigned int idx, void *arg)
+static int add_new_unspent(void *node, unsigned int index, void *arg)
 {
-	transaction_t *tx = node;
-	llist_t *new_unspent = ((llist_t **)arg)[0];
-	uint8_t *block_hash = ((uint8_t **)arg)[1];
-	unspent_tx_out_t *new_utxo;
+	transaction_t *tx = (transaction_t *)node;
+	llist_t *new_unspent = (llist_t *)arg;
 	int i;
-
-	(void)idx;
+	unspent_tx_out_t *unspent;
 
 	for (i = 0; i < llist_size(tx->outputs); i++)
 	{
-		new_utxo = unspent_tx_out_create(block_hash, tx->id,
-						 llist_get_node_at(tx->outputs, i));
-		if (!new_utxo || llist_add_node(new_unspent, new_utxo,
-						ADD_NODE_REAR) == -1)
-		{
-			free(new_utxo);
+		unspent = unspent_tx_out_create(((tx_out_t *)llist_get_node_at(tx->outputs, i)),
+										tx->id, NULL);
+		if (!unspent)
 			return (-1);
-		}
+		llist_add_node(new_unspent, unspent, ADD_NODE_REAR);
 	}
 	return (0);
 }
@@ -37,26 +31,17 @@ static int add_new_unspent(void *node, unsigned int idx, void *arg)
 /**
  * remove_spent_unspent - removes spent outputs from the list
  * @node: current unspent transaction output being checked
- * @idx: index of the unspent transaction output
  * @arg: list of validated transaction inputs
  * Return: 0 on success, 1 if the output should be removed
  */
-static int remove_spent_unspent(void *node, unsigned int idx, void *arg)
+static int remove_spent_unspent(void *node, void *arg)
 {
-	unspent_tx_out_t *utxo = node;
-	llist_t *inputs = arg;
-	tx_in_t *input;
-	int i;
+	tx_in_t *input = (tx_in_t *)arg;
+	unspent_tx_out_t *unspent = (unspent_tx_out_t *)node;
 
-	(void)idx;
-
-	for (i = 0; i < llist_size(inputs); i++)
-	{
-		input = llist_get_node_at(inputs, i);
-		if (!memcmp(utxo->tx_id, input->tx_id, SHA256_DIGEST_LENGTH) &&
-			!memcmp(utxo->block_hash, input->block_hash, SHA256_DIGEST_LENGTH))
-			return (1);
-	}
+	if (!memcmp(unspent->tx_id, input->tx_id, SHA256_DIGEST_LENGTH) &&
+	    !memcmp(unspent->out.hash, input->tx_out_hash, SHA256_DIGEST_LENGTH))
+		return (1);
 	return (0);
 }
 
@@ -72,7 +57,9 @@ llist_t *update_unspent(llist_t *transactions,
 			llist_t *all_unspent)
 {
 	llist_t *new_unspent;
-	void *args[2];
+	int i, j;
+	transaction_t *tx;
+	tx_in_t *input;
 
 	if (!transactions || !block_hash || !all_unspent)
 		return (NULL);
@@ -81,20 +68,19 @@ llist_t *update_unspent(llist_t *transactions,
 	if (!new_unspent)
 		return (NULL);
 
-	/* remove spent UTXOs */
-	llist_remove_node(all_unspent, remove_spent_unspent,
-			  transactions, 1, (node_dtor_t)free);
+	for (i = 0; i < llist_size(transactions); i++)
+	{
+		tx = llist_get_node_at(transactions, i);
 
-	/* carry over remaining unspent outputs */
-	llist_for_each(all_unspent, (node_func_t)llist_add_node,
-		       new_unspent);
+		for (j = 0; j < llist_size(tx->inputs); j++)
+		{
+			input = llist_get_node_at(tx->inputs, j);
+			llist_remove_node(all_unspent, (node_ident_t)remove_spent_unspent, input, 1, NULL);
+		}
+	}
 
-	/* add new UTXOs from processed txs */
-	args[0] = new_unspent;
-	args[1] = block_hash;
-	llist_for_each(transactions, add_new_unspent, args);
+	llist_for_each(transactions, (node_func_t)add_new_unspent, new_unspent);
 
 	llist_destroy(all_unspent, 1, (node_dtor_t)free);
-
 	return (new_unspent);
 }
