34482a9cfce42ec75dcf6646606e03aca022df25
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-19 20:14:08 -0500

    tests

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,1 +1,28 @@
+#include "transaction.h"
+
+/**
+ * tx_out_create - creates a new transaction output
+ * @amount: amount to be assigned to the transaction output
+ * @pub: receiver's public key
+ * Return: ptr to created transaction output on success, else NULL
+ */
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
+{
+	tx_out_t *out;
+	uint8_t data[sizeof(amount) + EC_PUB_LEN];
+
+	out = malloc(sizeof(tx_out_t)); /* alloc mem for transaction output */
+	if (!out)
+		return (NULL);
+	out->amount = amount; /* set amount, copy public key */
+	memcpy(out->pub, pub, EC_PUB_LEN);
+	memcpy(data, &amount, sizeof(amount)); /* hash prep */
+	memcpy(data + sizeof(amount), pub, EC_PUB_LEN);
+	if (!sha256(data, sizeof(data), out->hash))
+	{ /* generate hash of the amount and pub */
+		free(out);
+		return (NULL);
+	}
+	return (out);
+}
 
