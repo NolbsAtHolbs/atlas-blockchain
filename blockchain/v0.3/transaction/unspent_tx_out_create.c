000652a2221fb946616dec1d1e4adf7736ba1a02
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-19 21:10:07 -0500

    betty

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -12,7 +12,7 @@ unspent_tx_out_t *unspent_tx_out_create(
 					uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
 	unspent_tx_out_t *unspent;
-	
+
 	if (!block_hash || !tx_id || !out)
 		return (NULL);
 	unspent = malloc(sizeof(unspent_tx_out_t));
