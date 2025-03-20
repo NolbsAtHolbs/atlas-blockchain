bc168e8756966255cd17486dce339f647192122e
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-19 23:30:28 -0500

    refactor

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -6,54 +6,29 @@
  */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *bc = NULL;
-	block_t *block = NULL;
+	blockchain_t *bc = calloc(1, sizeof(*bc));
+	block_t *block;
 
-	bc = calloc(1, sizeof(*bc)); /* allocate blockchain structure */
-	if (!bc)
-		return (NULL);
+	/* allocate blockchain and create lists for chain and unspent outputs */
+	if (!bc || !(bc->chain = llist_create(MT_SUPPORT_FALSE)) ||
+		!(bc->unspent = llist_create(MT_SUPPORT_FALSE)))
+		return (free(bc), NULL);
 
-	bc->chain = llist_create(MT_SUPPORT_FALSE); /* create block chain list */
-	if (!bc->chain)
-	{
-		free(bc);
-		return (NULL);
-	}
-	/* create unspent outputs list */
-	bc->unspent = llist_create(MT_SUPPORT_FALSE);
-	if (!bc->unspent)
-	{
-		llist_destroy(bc->chain, 0, NULL);
-		free(bc);
-		return (NULL);
-	}
+	block = calloc(1, sizeof(*block));
+	if (!block) /* free everything if block allocation fails */
+		return (llist_destroy(bc->chain, 0, NULL),
+			llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
 
-	block = calloc(1, sizeof(*block)); /* create genesis block */
-	if (!block)
-	{
-		llist_destroy(bc->chain, 0, NULL);
-		free(bc);
-		return (NULL);
-	}
-
-	block->info.index = 0; /* initialize block info */
-	block->info.difficulty = 0;
-	block->info.timestamp = 1537578000;
-	block->info.nonce = 0;
-	memset(block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-
-	block->data.len = 16; /* initialize block data */
-	memcpy(block->data.buffer, "Holberton School", 16); /* wrong text */
-
-	/* set genesis block hash */
+	/* initialize genesis block fields */
+	block->info = (block_info_t){0, 0, 1537578000, 0, {0}};
+	block->data.len = 16;
+	memcpy(block->data.buffer, "Holberton School", 16); /* fixed 16-byte str */
 	memcpy(block->hash, ATLAS_SCHOOL_SHA256, SHA256_DIGEST_LENGTH);
 
+	/* add genesis block to chain; free on failure */
 	if (llist_add_node(bc->chain, block, ADD_NODE_REAR) == -1)
-	{ /* add genesis block to chain */
-		free(block);
-		llist_destroy(bc->chain, 0, NULL);
-		free(bc);
-		return (NULL);
-	}
+		return (free(block), llist_destroy(bc->chain, 0, NULL),
+			llist_destroy(bc->unspent, 0, NULL), free(bc), NULL);
+
 	return (bc);
 }
