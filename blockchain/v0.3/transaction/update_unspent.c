a521a7fed955b462ec69ded7d3530de6e5a804da
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-03-20 00:34:15 -0500

    wrong header

--- blockchain/v0.3/transaction/update_unspent.c
+++ blockchain/v0.3/transaction/update_unspent.c
@@ -1,39 +1,5 @@
-#include "blockchain.h"
+#include "transaction.h"
 
-static int remove_spent_unspent(void *node, void *arg);
-static int add_new_unspent(void *node, unsigned int idx, void *arg);
-
-/**
- * add_new_unspent - adds new unspent transaction outputs to list
- * @node: current transaction being processed
- * @index: index of the transaction
- * @arg: pointer to a struct containing block hash and new unspent list
- * Return: 0 on success, -1 on failure
- */
-static int add_new_unspent(void *node, unsigned int index, void *arg)
-{
-	transaction_t *tx = (transaction_t *)node;
-	llist_t *new_unspent = (llist_t *)arg;
-	int i;
-	unspent_tx_out_t *unspent;
-
-	for (i = 0; i < llist_size(tx->outputs); i++)
-	{
-		unspent = unspent_tx_out_create(((tx_out_t *)llist_get_node_at(tx->outputs, i)),
-										tx->id, NULL);
-		if (!unspent)
-			return (-1);
-		llist_add_node(new_unspent, unspent, ADD_NODE_REAR);
-	}
-	return (0);
-}
-
-/**
- * remove_spent_unspent - removes spent outputs from the list
- * @node: current unspent transaction output being checked
- * @arg: list of validated transaction inputs
- * Return: 0 on success, 1 if the output should be removed
- */
 static int remove_spent_unspent(void *node, void *arg)
 {
 	tx_in_t *input = (tx_in_t *)arg;
@@ -45,16 +11,39 @@ static int remove_spent_unspent(void *node, void *arg)
 	return (0);
 }
 
+static int add_new_unspent(void *node, unsigned int index, void *arg)
+{
+	transaction_t *tx = (transaction_t *)node;
+	llist_t *new_unspent = (llist_t *)arg;
+	int i;
+	unspent_tx_out_t *unspent;
+	tx_out_t *out;
+
+	(void)index; /* Suppress unused parameter warning */
+
+	for (i = 0; i < llist_size(tx->outputs); i++)
+	{
+		out = llist_get_node_at(tx->outputs, i);
+		if (!out)
+			continue;
+		unspent = unspent_tx_out_create(tx->id, tx->id, out);
+		if (!unspent)
+			return (-1);
+		llist_add_node(new_unspent, unspent, ADD_NODE_REAR);
+	}
+	return (0);
+}
+
 /**
- * update_unspent - Updates the list of all unspent transaction outputs
- * @transactions: List of validated transactions
- * @block_hash: Hash of the validated Block that contains the transaction list
- * @all_unspent: Current list of unspent transaction outputs
- * Return: A pointer to the new list of unspent transaction outputs
+ * update_unspent - updates unspent transaction outputs
+ * @transactions: list of validated transactions
+ * @block_hash: hash of the validated block
+ * @all_unspent: current list of unspent transaction outputs
+ * Return: new list of unspent transaction outputs
  */
 llist_t *update_unspent(llist_t *transactions,
-			uint8_t block_hash[SHA256_DIGEST_LENGTH],
-			llist_t *all_unspent)
+						uint8_t block_hash[SHA256_DIGEST_LENGTH],
+						llist_t *all_unspent)
 {
 	llist_t *new_unspent;
 	int i, j;
